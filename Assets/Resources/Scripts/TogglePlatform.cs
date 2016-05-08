5c61e1e1aff677c50f57e143048899b51c1cbeb8
Tadrith <wanderingMageA@yahoo.com>
2016-05-08 16:43:07 -0700

    Platform Toggle Works

--- Assets/Resources/Scripts/TogglePlatform.cs
+++ Assets/Resources/Scripts/TogglePlatform.cs
@@ -18,11 +18,11 @@ public class TogglePlatform : MonoBehaviour {
 			BoxCollider2D myColliderTop = gameObject.AddComponent<BoxCollider2D> ();
 			myColliderTop.offset = new Vector2 (-0.1697314f, 1.260993f);
 			myColliderTop.size = new Vector2 (3.828859f, 0.5147915f);
-			myColliderTop.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			myColliderTop.sharedMaterial = Resources.Load ("Physics Materials/Ground") as PhysicsMaterial2D;
 			BoxCollider2D myColliderBottom = gameObject.AddComponent<BoxCollider2D> ();
 			myColliderBottom.offset = new Vector2 (-0.1819516f, .9855719f);
 			myColliderBottom.size = new Vector2 (2.455615f, .7270756f);
-			myColliderBottom.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			myColliderBottom.sharedMaterial = Resources.Load ("Physics Materials/Ground") as PhysicsMaterial2D;
 		} else if (isActive == false) {
 			foreach (SpriteRenderer curChild in gameObject.GetComponentsInChildren<SpriteRenderer> ()) {
 				curChild.color = new Color (1f, 1f, 1f, .35f);
@@ -57,11 +57,11 @@ public class TogglePlatform : MonoBehaviour {
 			BoxCollider2D myColliderTop = gameObject.AddComponent<BoxCollider2D> ();
 			myColliderTop.offset = new Vector2 (-0.1697314f, 1.260993f);
 			myColliderTop.size = new Vector2 (3.828859f, 0.5147915f);
-			myColliderTop.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			myColliderTop.sharedMaterial = Resources.Load ("Physics Materials/Ground") as PhysicsMaterial2D;
 			BoxCollider2D myColliderBottom = gameObject.AddComponent<BoxCollider2D> ();
 			myColliderBottom.offset = new Vector2 (-0.1819516f, .9855719f);
 			myColliderBottom.size = new Vector2 (2.455615f, .7270756f);
-			myColliderBottom.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			myColliderBottom.sharedMaterial = Resources.Load ("Physics Materials/Ground") as PhysicsMaterial2D;
 			foreach (SpriteRenderer curChild in gameObject.GetComponentsInChildren<SpriteRenderer> ()) {
 				curChild.color = new Color (1f, 1f, 1f, 1f);
 			}
