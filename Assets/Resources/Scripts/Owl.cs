3b076a40d202ce64f3d0d770b151fd42cd0b200f
aarongu <aarongu@uw.edu>
2016-05-11 16:22:05 -0700

    added owl object, this beast checks if the player is under it and drops eggs onto him

---
+++ Assets/Resources/Scripts/Owl.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Owl : MonoBehaviour {
+	public enum State {
+		Patrol, Charge
+	}
+
+	public GameObject projectile = null;
+	public const float projectileInterval = .5f;
+
+	public LayerMask foxMask;
+	public SightScript sight;
+	private State curState;
+	private Rigidbody2D myRB;
+	private float myWidth;
+	private Vector3 returnPoint;
+	public GameObject player;
+	public float lineScalar;
+
+	public float speed;
+	public float distance;
+	public bool flip;
+
+	private float prevProj;
+
+
+	// Use this for initialization
+	void Start () {
+		prevProj = Time.realtimeSinceStartup;
+		flip = false;
+		speed = 4f;
+		myRB = gameObject.GetComponent<Rigidbody2D> ();
+		sight = gameObject.GetComponent<SightScript> ();
+		curState = State.Patrol;
+		myWidth = this.GetComponent<SpriteRenderer> ().bounds.extents.x;
+		player = GameObject.FindWithTag("Player");
+		if (null == projectile) {
+			projectile = Resources.Load ("Prefabs/Egg") as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (curState);
+		distance--;
+		if (!flip) {
+			gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+			gameObject.transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else {
+			gameObject.transform.Translate (Vector3.left * speed * Time.deltaTime);
+			gameObject.transform.localScale = new Vector3 (-1f, 1f, 1f);
+		}
+		if (distance <= 0) {
+			distance = 50;
+			flip = !flip;
+		}
+
+		switch (curState) {
+		case State.Charge:
+			updateFromCharge ();
+			break;
+		case State.Patrol:
+			updateFromPatrol();
+			break;
+		}
+	}
+
+	void updateFromCharge() {
+		float currentTime = Time.realtimeSinceStartup;
+		if (currentTime - prevProj > projectileInterval) {
+			prevProj = currentTime;
+			Vector2 lineCastPos = (gameObject.transform.position - player.transform.position).normalized;
+			GameObject e = Instantiate (projectile) as GameObject;
+			RockBehavior egg = e.GetComponent<RockBehavior> ();
+			if (null != egg) {
+				e.transform.position = transform.position;
+				egg.SetForwardDirection (lineCastPos);
+			}
+		}
+		curState = State.Patrol;
+	}
+
+	void updateFromPatrol() {
+		Debug.Log ("in patrol");
+		Vector2 lineCastPos = gameObject.transform.position - gameObject.transform.right * myWidth;
+		Debug.DrawLine (lineCastPos, lineCastPos + Vector2.down * lineScalar * 3);
+		bool playerSpotted = Physics2D.Linecast (lineCastPos, lineCastPos + Vector2.down * lineScalar * 3, foxMask);
+		Debug.Log (playerSpotted);
+		if (playerSpotted) {
+			curState = State.Charge;
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{		
+		if (other.gameObject.name == "Player") {
+			GameObject p =  GameObject.Find ("Player");
+			p.SendMessage ("decreaseHealth", .15f);
+		}
+	}
+
+}
---
+++ Assets/Resources/Scripts/RockBehavior.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class RockBehavior : MonoBehaviour {
+
+	private float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += (speed * Time.smoothDeltaTime) * transform.up;
+
+		if (!GetComponent<SpriteRenderer>().isVisible) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void SetForwardDirection(Vector3 f) {
+		transform.up = -f;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		// Only care if hitting an Egg (vs. hitting another Enemy!
+		if (other.gameObject.name == "Player") {
+			Destroy(this.gameObject);
+			GameObject p =  GameObject.Find ("Player");
+			p.SendMessage ("decreaseHealth", .10f);
+		}
+		else if (other.gameObject.layer == 8) {
+			Destroy(gameObject);
+		}
+	}
+
+
+}
