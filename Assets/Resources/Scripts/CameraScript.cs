44ed3c5be8e5eaf78309fc6c71e7e0b5f1b1334d
Andrew Watson <watsonap6@gmail.com>
2016-05-11 14:03:58 -0700

    Commented out borken lines in Camera Script. Removed Library files.

--- Assets/Resources/Scripts/CameraScript.cs
+++ Assets/Resources/Scripts/CameraScript.cs
@@ -17,25 +17,26 @@ public class CameraScript : MonoBehaviour {
 		animalCount = 0;
 		smoothTime = .05f;
 		player = GameObject.FindGameObjectWithTag ("Player");
+      Debug.Log(player.name);
 		minCameraPos = new Vector3 (6f, 4.5f, -10f);
 		maxCameraPos = new Vector3 (60f, 9.5f, -10f);
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-
+      /*
 		GameObject echoText = GameObject.Find ("numberScanned");
 		UnityEngine.UI.Text gui = echoText.GetComponent<UnityEngine.UI.Text> ();
-
+      
 		if (animalCount >= 2) {
 			gui.text = "WIN";
 		} else {
 			gui.text = "Animals Scanned " + animalCount;
 		}
-
+      */
 		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTime);
 		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTime);
-
+      
 		transform.position = new Vector3 (posX, posY, transform.position.z);
 
 		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x),
