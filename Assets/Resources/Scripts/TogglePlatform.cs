ddb1f84080e6c3792f002d73f00b1d587f4a81c6
candyhero <wchan008@uw.edu>
2016-05-11 02:23:13 -0700

    Level scene finished
    The remaining of the level scene completed.
    Lack fox class so have not put fox in yet.

--- Assets/Resources/Scripts/TogglePlatform.cs
+++ Assets/Resources/Scripts/TogglePlatform.cs
@@ -15,17 +15,15 @@ public class TogglePlatform : MonoBehaviour {
 		}
 
 		if (isActive == true) {
-			BoxCollider2D myColliderTop = gameObject.AddComponent<BoxCollider2D> ();
-			myColliderTop.offset = new Vector2 (-0.1697314f, 1.260993f);
-			myColliderTop.size = new Vector2 (3.828859f, 0.5147915f);
-			myColliderTop.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
-			BoxCollider2D myColliderBottom = gameObject.AddComponent<BoxCollider2D> ();
-			myColliderBottom.offset = new Vector2 (-0.1819516f, .9855719f);
-			myColliderBottom.size = new Vector2 (2.455615f, .7270756f);
-			myColliderBottom.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			foreach (BoxCollider2D curCollider in gameObject.GetComponents<BoxCollider2D> ()) {
+				curCollider.enabled = true;
+			}
 		} else if (isActive == false) {
 			foreach (SpriteRenderer curChild in gameObject.GetComponentsInChildren<SpriteRenderer> ()) {
 				curChild.color = new Color (1f, 1f, 1f, .35f);
+			}
+			foreach (BoxCollider2D curCollider in gameObject.GetComponents<BoxCollider2D> ()) {
+				curCollider.enabled = false;
 			}
 		}
 	}
@@ -46,22 +44,17 @@ public class TogglePlatform : MonoBehaviour {
 
 		if (isActive == true) {
 			isActive = false;
-			BoxCollider2D[] boxArray = gameObject.GetComponents<BoxCollider2D> ();
-			foreach (BoxCollider2D curCollider in boxArray)
-				Destroy (curCollider);
+			foreach (BoxCollider2D curCollider in gameObject.GetComponents<BoxCollider2D> ()) {
+				curCollider.enabled = false;
+			}
 			foreach (SpriteRenderer curChild in gameObject.GetComponentsInChildren<SpriteRenderer> ()) {
 				curChild.color = new Color (1f, 1f, 1f, .35f);
 			}
 		} else if (isActive == false) {
 			isActive = true;
-			BoxCollider2D myColliderTop = gameObject.AddComponent<BoxCollider2D> ();
-			myColliderTop.offset = new Vector2 (-0.1697314f, 1.260993f);
-			myColliderTop.size = new Vector2 (3.828859f, 0.5147915f);
-			myColliderTop.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
-			BoxCollider2D myColliderBottom = gameObject.AddComponent<BoxCollider2D> ();
-			myColliderBottom.offset = new Vector2 (-0.1819516f, .9855719f);
-			myColliderBottom.size = new Vector2 (2.455615f, .7270756f);
-			myColliderBottom.sharedMaterial = Resources.Load ("Physical Material/Ground") as PhysicsMaterial2D;
+			foreach (BoxCollider2D curCollider in gameObject.GetComponents<BoxCollider2D> ()) {
+				curCollider.enabled = true;
+			}
 			foreach (SpriteRenderer curChild in gameObject.GetComponentsInChildren<SpriteRenderer> ()) {
 				curChild.color = new Color (1f, 1f, 1f, 1f);
 			}
