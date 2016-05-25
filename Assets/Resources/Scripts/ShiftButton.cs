c1636207f16671a97eb341553c292eac3e27cdab
aarongu <aarongu@uw.edu>
2016-05-24 20:41:25 -0700

    added shift sign that blinks in the first part of the map and in the maze to alert the player they can time shift. added signs for snowman puzzle telling user they can move the block and platform. tried fixing the top right and top left bears in the first part of the map by tagging the walls as wall but they still dont turn around

---
+++ Assets/Resources/Scripts/ShiftButton.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShiftButton : MonoBehaviour {
+	public Sprite button;
+
+	private SpriteRenderer rend;
+	private IEnumerator coroutine;
+
+	private bool playerCollide;
+	private bool appear;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+		playerCollide = false;
+		appear = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerCollide) {
+			if (!appear) {
+				rend.sprite = button;
+				appear = true;
+				coroutine = Blink (rend, 10, 1f, true);
+				StartCoroutine (coroutine);
+			}
+
+		} else {
+			rend.sprite = null;
+			StopCoroutine (coroutine);
+			appear = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			playerCollide = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Player") {
+			playerCollide = false;
+		}
+	}
+
+	IEnumerator Blink(SpriteRenderer sprite, int numTimes, float delay, bool disabled) {
+		for (int i = 0; i < numTimes; i++) {
+			if (disabled) {
+				sprite.enabled = false;
+			} else {
+				sprite.color = new Color (sprite.color.r, sprite.color.g, sprite.color.b, .5f);
+			}
+
+			yield return new WaitForSeconds (delay);
+
+			if (disabled) {
+				sprite.enabled = true;
+			} else {
+				sprite.color = new Color (sprite.color.r, sprite.color.g, sprite.color.b, 1);
+			}
+
+			yield return new WaitForSeconds (delay);
+		}
+	
+	}
+}
